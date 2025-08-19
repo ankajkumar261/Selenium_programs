0d7d9887500b5f9278fb6d8f9c3538d55eed8534
Ankajk <Ankajkumar261@gmail.com>
2025-08-19 23:29:12 +0530

    new push

---
+++ src/test/java/com/Ankajkuamr/SeleniumBasics/ex01_selenium.java
@@ -0,0 +1,17 @@
+package com.Ankajkuamr.SeleniumBasics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class ex01_selenium {
+
+
+    @Test
+    public void testcase1(){
+
+        Assert.assertEquals("Ankaj","Ankaj");
+
+
+    }
+}
