0971eb75223b65314d9cbfa53f24b608f56c1637
Ankajk <Ankajkumar261@gmail.com>
2025-09-10 22:51:08 +0530

    push

---
+++ src/test/java/Waits/Ex_12_waits.java
@@ -0,0 +1,19 @@
+package Waits;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Ex_12_waits {
+
+    public static void main(String[] args) {
+
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        webDriver.get("https://app.vwo.com");
+
+
+    }
+}
---
+++ src/test/java/Xpath/Ex_11_Xpath.java
@@ -0,0 +1,29 @@
+package Xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+public class Ex_11_Xpath {
+
+    public static void main(String[] args) {
+
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("https://app.vwo.com/#/login");
+
+
+        WebElement username = webDriver.findElement(By.xpath("//Input [@id =\"login-username\"]"));
+        username.sendKeys("dkjasdjk");
+
+        WebElement password = webDriver.findElement(By.xpath("//Input [@id=\"login-password\"]"));
+           password.sendKeys("kjvgujydghbv");
+
+
+
+        webDriver.quit();
+
+
+    }
+}
